e48e93303f5099eb7866acac4a055a1c8c919b41
Michał Jarmużek <michalsmigiel78@gmail.com>
2020-01-19 16:24:12 +0100

    add signup and login function

---
+++ waterappapi.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'tojesttajnyklucz'
+Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=90)])
+    remember = BooleanField('pamiętaj mnie')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=90)])
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+@app.route('/signup')
+def signup():
+    form = RegistrationForm()
+    return render_template('signup.html', form=form)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
