ba8c05e7f187bc17cce981add31ccd287063da1d
Michał Jarmużek <michalsmigiel78@gmail.com>
2020-01-25 11:06:35 +0100

    add jwt

--- waterappapi.py
+++ waterappapi.py
@@ -1,82 +1,48 @@
-from flask import Flask, render_template, redirect, url_for, jsonify
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-#zabezpiecznie hasla
+from uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'tojesttajnyklucz'
+
+app.config['SECRET_KEY'] = 'kluczniejawny'
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:pg@localhost/waterapp'
-Bootstrap(app)
+
 db = SQLAlchemy(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email= db.Column(db.String(50), unique=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
     password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(50))
+    complete = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer)
 
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=90)])
-    remember = BooleanField('pamiętaj mnie')
+@app.route('/user', method=['GET'])
+def get_all_users():
+    return ''
 
-class RegistrationForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=90)])
+@app.route('/user/<user_id>', method=['GET'])
+def get_one_user():
+    return ''
 
-@app.route('/')
-def index():
-    #return render_template('index.html')
-    return jsonify({'message':'/login,/signup,/logout,/dashboard'})
+@app.route('/user', method=['POST'])
+def create_user():
+    return ''
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data): #odhashowanie hasla zapisanego w bazie
-                login_user(user, remember = form.remember.data)
-                return redirect(url_for('dashboard'))
-        return jsonify({'message': 'niewlasciwa nazwa uzytkownika albo haslo'})
-    return render_template('login.html', form=form)
+@app.route('/user/<user_id>', method=[PUT])
+def promote_user():
+    return ''
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256') #hashowanie hasla do zapisania w bazie
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify({'message': 'nowy uzytkownik zostal utworzony'})
-    return render_template('signup.html', form=form)
-
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return jsonify({'message': 'strona tylko dla zalogowanych uzytkownikow'})
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
+@app.route('/user/<user_id>', method=['DELETE'])
+def delete_user():
+    return ''
 
 if __name__ == '__main__':
     app.run(debug=True)
