62a5f5171886747569efa30c42f6070e01863fb6
Michał Jarmużek <michalsmigiel78@gmail.com>
2020-01-25 09:02:36 +0100

    add signup, signin, logout function

--- waterappapi.py
+++ waterappapi.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, jsonify
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+#zabezpiecznie hasla
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'tojesttajnyklucz'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:pg@localhost/waterapp'
 Bootstrap(app)
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email= db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -19,24 +39,44 @@ class RegistrationForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=90)])
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
+    return jsonify({'message':'/login,/signup,/logout,/dashboard'})
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data): #odhashowanie hasla zapisanego w bazie
+                login_user(user, remember = form.remember.data)
+                return redirect(url_for('dashboard'))
+        return jsonify({'message': 'niewlasciwa nazwa uzytkownika albo haslo'})
     return render_template('login.html', form=form)
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256') #hashowanie hasla do zapisania w bazie
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'nowy uzytkownik zostal utworzony'})
     return render_template('signup.html', form=form)
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html')
+    return jsonify({'message': 'strona tylko dla zalogowanych uzytkownikow'})
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
