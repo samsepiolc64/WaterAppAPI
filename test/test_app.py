65bb0e3be74fbc62ce4fede89e8d68d0d8b50078
Michał Jarmużek <michalsmigiel78@gmail.com>
2020-02-16 17:05:59 +0100

    add next tests

--- test/test_app.py
+++ test/test_app.py
@@ -26,3 +26,23 @@ def test_get_all_users():
         headers={'x-access-token': auth_token}
     )
     assert response.status_code == 200
+
+def test__get_one_user():
+    auth_token = get_auth_token()
+    response = requests.get(
+        'http://127.0.0.1:5000/user/ef907b49-df31-44b4-b07d-7fab86af461b',
+        headers={'x-access-token': auth_token}
+    )
+    assert response.status_code == 200
+
+def test_get_one_user():
+    auth_token = get_auth_token()
+    response = requests.get(
+        'http://127.0.0.1:5000/user/ef907b49-df31-44b4-b07d-7fab86af461b',
+        headers={'x-access-token': auth_token}
+    )
+    data = response.json()
+    assert data.get('user')['email'] == 'ala@wp.pl'
+
+
+
