9d7aeaa523e5a94a4a80acbd6d32ec56526b34df
Michał Jarmużek <michalsmigiel78@gmail.com>
2020-02-19 19:58:06 +0100

    add new tests

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import sys
 sys.path.append("./views")
 
 app = Flask(__name__)
-
 app.config.from_pyfile('./config/config.py')
 
 from views.views import *
 
 if __name__ == '__main__':
      app.run()
+     
